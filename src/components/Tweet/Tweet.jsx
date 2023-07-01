01fa09b654439ff6e62c6cdeab7ce79fbda05449
José Eduardo Ormeño Meneses <jormenom@odybank.com.pe>
2023-06-30 19:15:31 -0500

    aling justify container

--- src/components/Tweet/Tweet.jsx
+++ src/components/Tweet/Tweet.jsx
@@ -9,9 +9,6 @@ const Tweet = () => {
         <ItemTweet />
         <ItemTweet />
         <ItemTweet />
-        <ItemTweet />
-        <ItemTweet />
-        <ItemTweet />
       </div>
     </div>
   );
